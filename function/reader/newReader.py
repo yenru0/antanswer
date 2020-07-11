f13d507a298fd0a0212766d469d2edc2d03cbbee
yenru0 <36220355+yenru0@users.noreply.github.com>
2020-07-12 02:10:37 +0900

    modify ebnf of newReader.py

--- function/reader/newReader.py
+++ function/reader/newReader.py
@@ -2,24 +2,31 @@
 antanswer에 대한 EBNF
 
 
+<Filter> ::= <Stage>*
 
-Filter ::= <표현셀>
+<StageSSKeyword> ::= "##@"
+<CommandSSKeyword> ::= "##!"
+<LetSSKeyword> ::= "##$"
+<InlineCommentSSKeyword> "###"
 
-StageSSKeyword ::= '##@'
-LineBreak ::= '\n'
+<LineBreak> ::= "\n"
+<L3Sep> ::= ";"
+<L2Sep> ::= "|"
+<L1Sep> ::= ":"
 
-<L3구분자> ::= ';'
-<L2구분자> ::= '|'
-<L1구분자> ::= ':'
+<expressLetter> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | " "
+<InlineexpressCell> ::= <expressLetter>*
+<expressCell> ::= <expressLetter>* | "{" <expressLetter>* { <expressLetter>* <LineBreak> } "}"
 
-<표현문자> ::= '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | ' '
-<Inline표현셀> ::= 표현문자*
-<표현셀> ::= <표현문자>* | '{' <표현문자>*{<표현문자>* LineBreak} '}'
+<SubSub> ::= <expressCell>{<L3Sep><expressCell>}[<L3Sep>] | "{" LineBreak* <expressCell>{<L3Sep><expressCell>}[<L3Sep>] LineBreak* "}"
 
-<SubSub> ::= <표현셀>{<L3구분자><표현셀>}[<L3구분자>] | '{' LineBreak* <표현셀>{<L3구분자><표현셀>}[<L3구분자>] LineBreak* '}'
-<Sub> ::= <SubSub>{<L2구분자><SubSub>}[<L2구분자>] | '{' LineBreak* <SubSub>{<L2구분자><SubSub>}[<L2구분자>] LineBreak* '}'
-<Element> ::= <Sub>{<L1구분자><Sub>}[<L1구분자>] | '{' LineBreak* <Sub>{<L1구분자><Sub>}[<L1구분자>] LineBreak* '}'
-<Stage> ::= <StageSSKeyword><표현문자><LineBreak>{<Element><LineBreak>}
+<Sub> ::= <SubSub>{<L2Sep><SubSub>}[<L2Sep>] | "{" LineBreak* <SubSub>{<L2Sep><SubSub>}[<L2Sep>] LineBreak* "}"
+
+<Element> ::= <Sub>{<L1Sep><Sub>}[<L1Sep>] LineBreak | "{" LineBreak* <Sub>{<L1Sep><Sub>}[<L1Sep>] LineBreak* "}"
+
+<Stage> ::= <StageSSKeyword><InlineexpressCell><LineBreak>{<Element>{<LineBreak>}}
+    | <StageSSKeyword><InlineexpressCell><expressLetter> "{" {<Element>{<LineBreak>}} "}" {LineBreak}
+
 
 
 
